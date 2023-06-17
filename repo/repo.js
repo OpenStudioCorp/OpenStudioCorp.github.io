1d479d1b3c88dce876734b667ab4878693ad1fcc
charlie-sans <gamerboy030607@gmail.com>
2023-06-17 22:21:11 +1000

    create repo.html, repo.css and repo.js

--- repo/repo.js
+++ repo/repo.js
@@ -1,60 +1,64 @@
 // Get the container element
 const container = document.getElementById('container');
 
-// Fetch the JSON file
-fetch('repo.json')
-  .then(response => response.json())
-  .then(data => {
-    // Loop through the data and create a checkbox for each entry
-    data.packages.forEach(entry => {
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.value = entry.name;
-      container.appendChild(checkbox);
+// Get the button element
+const button = document.querySelector('button');
 
-      const label = document.createElement('label');
-      label.textContent = `${entry.name} (${entry.version})`;
-      container.appendChild(label);
+if (button) {
+  // Fetch the JSON file
+  fetch('repo.json')
+    .then(response => response.json())
+    .then(data => {
+      // Loop through the data and create a checkbox for each entry
+      data.packages.forEach(entry => {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.value = entry.name;
+        checkbox.id = entry.name;
+        container.appendChild(checkbox);
 
-      container.appendChild(document.createElement('br'));
-    });
+        const label = document.createElement('label');
+        label.textContent = `${entry.name} (${entry.version})`;
+        label.setAttribute('for', entry.name);
+        container.appendChild(label);
 
-    // Create a button to download the selected entries
-    const button = document.createElement('button');
-    button.textContent = 'Download Selected Entries';
-    button.style.display = 'none';
-    button.addEventListener('click', () => {
-      const selectedEntries = [];
+        container.appendChild(document.createElement('br'));
+      });
+
+      // Create a button to download the selected entries
+      button.textContent = 'Download Selected Entries';
+      button.style.display = 'none';
+      button.addEventListener('click', () => {
+        const selectedEntries = [];
+        const checkboxes = container.querySelectorAll('input[type="checkbox"]');
+        if (checkboxes && checkboxes.length > 0) {
+          checkboxes.forEach(checkbox => {
+            if (checkbox.checked) {
+              selectedEntries.push(checkbox.value);
+            }
+          });
+        }
+
+        const content = selectedEntries.join('\n');
+        const blob = new Blob([content], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'selected-entries.txt';
+        link.click();
+        URL.revokeObjectURL(url);
+      });
+
+      // Show the button if any checkboxes are checked
       const checkboxes = container.querySelectorAll('input[type="checkbox"]');
       if (checkboxes && checkboxes.length > 0) {
         checkboxes.forEach(checkbox => {
-          if (checkbox.checked) {
-            selectedEntries.push(checkbox.value);
-          }
+          checkbox.addEventListener('change', () => {
+            const anyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+            button.style.display = anyChecked ? 'block' : 'none';
+          });
         });
       }
-
-      const content = selectedEntries.join('\n');
-      const blob = new Blob([content], { type: 'text/plain' });
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = 'selected-entries.txt';
-      link.click();
-      URL.revokeObjectURL(url);
-    });
-
-    container.appendChild(button);
-
-    // Show the button if any checkboxes are checked
-    const checkboxes = container.querySelectorAll('input[type="checkbox"]');
-    if (checkboxes && checkboxes.length > 0) {
-      checkboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', () => {
-          const anyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
-          button.style.display = anyChecked ? 'block' : 'none';
-        });
-      });
-    }
-  })
-  .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
+}
